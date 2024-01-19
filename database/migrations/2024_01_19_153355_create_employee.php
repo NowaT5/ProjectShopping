a2c03e5aaf1f0e35ce00a2b01f1f73cd16ef5d85
NowaT5 <nowa2536@gmail.com>
2024-01-19 22:39:12 +0700

    update database

--- database/migrations/2024_01_19_153355_create_employee.php
+++ database/migrations/2024_01_19_153355_create_employee.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateEmployee extends Migration
 {
     /**
      * Run the migrations.
@@ -13,22 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('employee', function (Blueprint $table) {
             $table->id();
             $table->string('fname');
             $table->string('lname');
-            $table->string('age');
-            $table->text('address');
-            $table->string('subdistrict_id');
-            $table->string('district_id');
-            $table->string('province_id');
-            $table->string('zipcode');
-            $table->string('phone');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->char('username');
             $table->string('password');
-            $table->rememberToken();
+            $table->char('phone');
+            $table->char('age');
+            $table->char('gender');
+            $table->string('emtype_id');
             $table->timestamps();
         });
     }
@@ -40,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('employee');
     }
 }
